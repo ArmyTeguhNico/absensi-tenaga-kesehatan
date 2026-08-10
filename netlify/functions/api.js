fc81980b3fe40242f2d66e3bd19c1decf782fe82
PKMKOJU <armyteguh00@gmail.com>
2026-08-10 11:56:53 +0700

    Fix CORS origin for absensitenkesrs domain

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -17,6 +17,7 @@ const app = express();
 app.use(cors({
   origin: [
     'http://localhost:3000',
+    'https://absensitenkesrs.netlify.app',
     'https://absensitenkes.netlify.app'
   ],
   credentials: true,
